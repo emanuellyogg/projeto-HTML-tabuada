bf5c2dc71f6535267d36371fe8f5a85424116496
unknown <manuogg90@gmail.com>
2021-06-17 12:51:03 -0300

    feat: tabuada

---
+++ assets/js/todo.js
@@ -0,0 +1,18 @@
+let data = [{ //criando uma Array de json (objetos)
+  id: 1,
+  title: "Levantar as 07:00 horas"
+}, {
+  id: 2,
+  title: "café da manhã saudável"
+}, {
+  id: 3,
+  title: "treinar 15 min"
+}, {
+  id: 4,
+  title: "Estudar"
+}, {
+  id: 5,
+  title: "Organizar a casa"
+}];
+
+console.log(data);
